7bf55562e58ba71accbfe8576a4e1da689121dbc
Darkaos-5 <85962827+Darkaos-5@users.noreply.github.com>
2025-08-19 19:22:23 -0300

    Add files via upload
    Instancia inicial de projeto, primeiro "commit" feito.

---
+++ AulaAWT/NewMavenProject/src/main/java/com/mycompany/newmavenproject/NewMavenProject.java
@@ -0,0 +1,46 @@
+package com.mycompany.newmavenproject;
+
+/**
+ *
+ * @author Jonatan
+ */
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.*;
+
+public class NewMavenProject {
+
+    public static void main(String[] args){
+        JFrame Janela01 = new JFrame();
+        JLabel Texto = new JLabel("Olá Mundo.");
+        Janela01.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Janela01.setVisible(true);
+        Janela01.setSize (600, 400);
+        Janela01.setLocationRelativeTo(null);
+        Janela01.setTitle("Janela 01");
+        
+        JButton btnAbrir = new JButton("Abrir");
+        btnAbrir.setBounds(140, 170, 160, 30);
+        
+        Janela01.add(Texto);
+        Janela01.add(btnAbrir);
+        
+        btnAbrir.addActionListener(new ActionListener(){
+            public void actionPerformed (ActionEvent e){
+                AbrirJanela();
+            }
+        });
+        
+    }
+    
+    
+    public static void AbrirJanela(){
+        JFrame Janela02 = new JFrame();
+        Janela02.setSize(400, 400);
+        Janela02.setTitle("Janela 02");
+        Janela02.setVisible(true);
+        Janela02.setLocationRelativeTo(null);        
+        
+    }    
+}
